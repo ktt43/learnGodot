d01bb48c39fd9d3ca5cf11f0d0708058897a3a46
ktt43 <ktt43@rutgers.edu>
2026-03-19 17:33:10 -0400

    direction and camera2d

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -17,13 +17,18 @@ func _on_gate_player_entered_gate(body) -> void:
 	print("Called from Level")
 	print(body)
 
-func _on_player_laser_shot(pos) -> void:
-	var laser = laser_scene.instantiate()
-	laser.position= pos
+# Listens for Event from Player and Create the instance at the Position that is emitted, then add it to the Level
+func _on_player_laser_shot(pos, direction) -> void:
+	var laser = laser_scene.instantiate() as Area2D
+	
+	laser.position = pos
+	laser.rotation_degrees = rad_to_deg(direction.angle())+90
+	laser.direction = direction
 	$Projectiles.add_child(laser)
 	
-func _on_player_grenade_thrown(pos) -> void:
-	var grenade = grenade_scene.instantiate()
+func _on_player_grenade_thrown(pos, direction) -> void:
+	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
+	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
 	
--- scenes/projectiles/grenade.gd
+++ scenes/projectiles/grenade.gd
@@ -1,7 +1,3 @@
 extends RigidBody2D
 
-var direction: Vector2 = Vector2.UP
-var speed: int = 200
-
-func _process(delta: float) -> void:
-	position += speed * direction * delta
+var speed: int = 400
